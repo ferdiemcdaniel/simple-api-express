1b9cda2d60c1a73e64bca3708f785c90e3785e9f
Ferdinand McDaniel <ferdinanda@1day.io>
2019-04-01 13:08:47 +0800

    updated the return data when reference number input is an array

--- src/resources/booking/booking.controllers.js
+++ src/resources/booking/booking.controllers.js
@@ -4,7 +4,7 @@ import logger from '../../utils/logger'
 
 const extractReferenceNumber = req => {
   const { payload } = req.body
-  let toReturn = ''
+  let toReturn = []
   if (payload && typeof payload === 'string') {
     const { events } = JSON.parse(payload)
     events.forEach(({ message }) => {
@@ -15,7 +15,7 @@ const extractReferenceNumber = req => {
         )
         const { reference } = JSON.parse(extractedJson)
         if (reference && typeof reference === 'string') {
-          toReturn = reference
+          toReturn.push(reference)
         }
       }
     })
@@ -35,14 +35,19 @@ const maskResult = status => {
 }
 
 const newBooking = (req, res) => {
-  let referenceNumber = extractReferenceNumber(req)
+  let referenceNumbers = extractReferenceNumber(req)
   let auth = {
     username: config.secrets.temp.user,
     password: config.secrets.temp.pass
   }
-  ddpClient(config.backend.ci, auth, referenceNumber, (error, success) => {
+  ddpClient(config.backend.ci, auth, referenceNumbers, (error, success) => {
     let status = ''
-    if (error || success === null) {
+    if (
+      error ||
+      success === null ||
+      success.result === undefined ||
+      success.result.length === 0
+    ) {
       status = maskResult(200)
       let msg = `Vader Log: ${config.rootUrl} ${JSON.stringify({
         event: 'Validated New Booking',
@@ -56,31 +61,42 @@ const newBooking = (req, res) => {
         }
       })
     } else {
-      status = maskResult(success.result)
-      let msg = `Vader Log: ${config.rootUrl} ${JSON.stringify({
-        event: 'Validated New Booking',
-        status,
-        message: 'Booking FOUND'
-      })}`
-      logger.property_program('Makati Riverside Inn', 'info', msg)
-      res.status(200).json({
-        reservationsresponse: {
-          status
-        }
-      })
+      let toReturn = []
+      if (Array.isArray(success.result)) {
+        success.result.forEach(status => {
+          let reservationStatus = maskResult(status.status)
+          let msgData = {
+            event: 'Validated New Booking',
+            status: reservationStatus,
+            message: 'Booking FOUND',
+            'check-in-date': status.checkin_date,
+            'check-out-date': status.checkout_date,
+            guest: status.guest
+          }
+          let msg = `Vader Log: ${config.rootUrl} ${JSON.stringify(msgData)}`
+          logger.property_program(status.property_name, 'info', msg)
+          toReturn.push(msgData)
+        })
+      }
+      res.status(200).json({ data: toReturn })
     }
   })
 }
 
 const cancelledBooking = (req, res) => {
-  let referenceNumber = extractReferenceNumber(req)
+  let referenceNumbers = extractReferenceNumber(req)
   let auth = {
     username: config.secrets.temp.user,
     password: config.secrets.temp.pass
   }
-  ddpClient(config.backend.ci, auth, referenceNumber, (error, success) => {
+  ddpClient(config.backend.ci, auth, referenceNumbers, (error, success) => {
     let status = ''
-    if (error || success === null) {
+    if (
+      error ||
+      success === null ||
+      success.result === undefined ||
+      success.result.length === 0
+    ) {
       status = maskResult(200)
       let msg = `Vader Log: ${config.rootUrl} ${JSON.stringify({
         event: 'Validated Cancelled Booking',
@@ -94,18 +110,24 @@ const cancelledBooking = (req, res) => {
         }
       })
     } else {
-      status = maskResult(300)
-      let msg = `Vader Log: ${config.rootUrl} ${JSON.stringify({
-        event: 'Validated Cancelled Booking',
-        status,
-        message: 'Booking FOUND'
-      })}`
-      logger.property_program('Makati Riverside Inn', 'info', msg)
-      res.status(200).json({
-        reservationsresponse: {
-          status
-        }
-      })
+      let toReturn = []
+      if (Array.isArray(success.result)) {
+        success.result.forEach(status => {
+          let reservationStatus = maskResult(status.status)
+          let msgData = {
+            event: 'Validated Cancelled Booking',
+            status: reservationStatus,
+            message: 'Booking FOUND',
+            'check-in-date': status.checkin_date,
+            'check-out-date': status.checkout_date,
+            guest: status.guest
+          }
+          let msg = `Vader Log: ${config.rootUrl} ${JSON.stringify(msgData)}`
+          logger.property_program(status.property_name, 'info', msg)
+          toReturn.push(msgData)
+        })
+      }
+      res.status(200).json({ data: toReturn })
     }
   })
 }
